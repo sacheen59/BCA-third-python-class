94688d66ee2541f48df07706a5d912caafd73b26
sachin barali <sachinbarali41@gmail.com>
2024-04-08 10:44:13 +0545

    operator

---
+++ operators.py
@@ -0,0 +1,45 @@
+# airthematic operator => +,- , /, *, %, //,**
+
+print(5/2)
+print(5//2)
+
+# ** => it is used to find the power of number
+print(5**2)
+print(2**3)
+
+# assignment operator=> =, +=, -= , /=, *= ...
+
+x= 0
+x+=3
+print(x)
+
+# comparision operator => used to compare between two operand
+# ==, < , > , <= ,>=, !=
+
+print(5==3)
+print(5>3)
+print(5!=3)
+
+# logical operator => it is used to compare two conditions(&&, ||, ! in other programming language )
+# and, or, not(in python)
+print("==============")
+print(5>3 and 5==3)
+print(5>3 or 5==3)
+print(not (5>3 or 5==3))
+
+# identity operator => is, is not
+print("===== identity operator ===")
+a = 3
+b = 3
+c = a
+print(a == b)
+print(a is not b )
+
+# membership operator => in , not in
+print("==== membership operator ===")
+number = [1,2,3,4,5]
+print(2 in number)
+print(6 in number)
+print(5 not in number)
+
+
