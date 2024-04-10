44d6d8bc8cb98287f27f1dd9742059c1d2482549
sachin barali <sachinbarali41@gmail.com>
2024-04-10 10:00:39 +0545

    day7

---
+++ forloop.py
@@ -0,0 +1,14 @@
+# fruits = ["apple","mango","banana"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# print(fruits)
+
+# #range(start,stop,step)
+
+# for i in range(0,10,2):
+#     print(i)
+
+for j in range(1,50,2):
+    print(j)
---
+++ whileloop.py
@@ -0,0 +1,19 @@
+x = 1
+
+# while loop
+while x < 10:
+    x+=1
+    if x == 3:
+        print("we are learning python")
+        # continue
+        break
+    # increment/decrement
+    print("we are learning python",x)
+else:
+    print(x)
+
+# for i in range(9):
+#     i+=1
+#     if i == 3:
+#         continue
+#     print(i)
