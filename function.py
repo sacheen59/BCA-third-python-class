92af759483c038181946ade0d44a009e7d6b7f70
sachin barali <sachinbarali41@gmail.com>
2024-04-15 09:49:34 +0545

    function

---
+++ function.py
@@ -0,0 +1,27 @@
+
+# def sum():
+#     x = 10
+#     y = 20
+#     print(x + y)
+
+# sum()
+# sum()
+
+# def mul(x,y):
+#     print(x*y)
+
+# mul(10,20)
+# mul(20,30)
+
+# named argument and default value in argument
+# def information(name,address="chitwan"):
+#     print("name of user is " + name + " He is from " + address)
+
+# information(name = "Ujjwal")
+
+def sum(x , y):
+    add = x+ y
+    return add
+
+a = sum(10,20)
+print(a)
