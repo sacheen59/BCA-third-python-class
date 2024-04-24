2b0f7a8e641b42431925993430c23d94ee237b9b
sachin barali <sachinbarali41@gmail.com>
2024-04-24 09:52:09 +0545

    list

---
+++ list.py
@@ -0,0 +1,12 @@
+# list=> list is the collection of ordered data that is changeable,allow duplication and which can be manipulate
+
+fruits = ["apple","banana","pineapple","mango","papaya"]
+print(type(fruits))
+
+# to find the length of an list 
+print(len(fruits))
+
+# accessing the list element
+print(fruits[0])
+print(fruits[2])
+print(fruits[1:])
